3480e61e0eb6b3af9f5f8d85f40f91095bc58bc4
jembach <jonas-embach@web.de>
2017-07-03 11:08:47 +0200

    added commentation

--- form.class.php
+++ form.class.php
@@ -2,11 +2,12 @@
 
 class form {
 
+
+  public static $supported=array("input","file","password","radio","checkbox","select","textarea","hidden");
   protected static $instance=0;
-	protected static $elements=array();
-  protected static $caption;
-  protected static $emptyObjects=array();
-  protected static $supported=array("input","file","password","radio","checkbox","select","textarea","hidden");
+	protected var $elements=array();
+  protected var $caption;
+  protected var $emptyObjects=array();
 
   const FORM_DATE=1;
   const FORM_IMAGE=2;
@@ -15,33 +16,40 @@ class form {
   const FORM_PLZ="/[0-9]{5}/";
   const FORM_NAME=6;
 
+
   /**
-   * setzt den Namen des Formulars
-   * @param  string $caption Name des Formulars
+   * initializer - optional could set the formular caption
+   *
+   * @param      string  $caption  The caption of the formular
    */
   public function __constuct($caption=""){
     self::$instance++;
-    self::$caption=$caption;
+    $this->caption=$caption;
   }
 
+
   /**
-   * löscht die gespeicherten Felder
+   * destructor - deletes the form elements
    */
   public function __destruct(){
-    self::$elements=array();
+    $this->elements=array();
   }
 
+
   /**
-   * Funktion zum hinzufühgen neuer Formularfelder
-   * @param string  $type     Typ des Feldes
-   * @param string  $name     Feldname
-   * @param string  $caption  Anzeigename des Feldes
-   * @param string  $value    Wert des Feldes
-   * @param boolean $required Prüfvariable, ob eine Eingabe erfolgen muss
-   * @param array   $list     Eingabeliste für bestimmte Felder
+   * adds a html formular input 
+   *
+   * @param      string   $type      The input type - only supported allowed
+   * @param      string   $name      The name for input element
+   * @param      <type>   $caption   The caption
+   * @param      string   $value     The value
+   * @param      boolean  $required  if set to false element is optional in the formular
+   *                                 - set to true for required or set an regex which is required
+   *                                 - or use a defined regex from the class constant
+   * @param      array    $list      The list for the select, radio or checkbox elements
    */
   public function add($type,$name,$caption,$value="",$required=false,$list=array()){
-    if(in_array($type,self::$supported) && !isset(self::$elements[$name])){
+    if(in_array($type,self::$supported) && !isset($this->elements[$name])){
       $regex="";
       if($required===true){
         $regex="/[^\ņ]+/";
@@ -49,38 +57,65 @@ class form {
         $regex=$required;
         $required=true;
       }
-      self::$elements[$name]=array("caption"=>$caption,"type"=>$type,"required"=>$required,"regex"=>$regex,"value"=>$value);
-      if(count($list)!=0) self::$elements[$name]["list"]=$list;
+      $this->elements[$name]=array("caption"=>$caption,"type"=>$type,"required"=>$required,"regex"=>$regex,"value"=>$value);
+      if(count($list)!=0) $this->elements[$name]["list"]=$list;
     }
   }
 
+
   /**
-   * Parserfunktion zum parsen der Formulardaten 
-   * @param  string  $file   Dateipfad
-   * @param  array   $Blocks liste der Werte die ersetzt werden sollen
-   * @param  boolean $return Prüfvariable, ob Daten ausgegeben oder zurückgegeben werden sollen
-   * @return [type]          die geparste Datei
+   * checks if all required elements were successfull send
+   *
+   * @return     boolean  if true all required elements where send
    */
-  public function parse($file,$Blocks=array(),$return=false){
-    if(!file_exists($file)){
-      return false;
-    } else {
-      $data=file_get_contents($file);
-      foreach($Blocks as $block => $replace){
-        $data = str_replace("{__".$block."__}", $replace, $data);
+  public function check(){
+    foreach ($this->elements as $name => $value) {
+      if($value['required']===true && (!isset($_POST[$name]) || empty($_POST[$name]) || !preg_match($value['regex'], $_POST[$name]))){
+        $this->emptyObjects[]=$name;
       }
-      if($return) return $data;
-      else        echo $data;
     }
+    return (count($this->emptyObjects)==0);
   }
 
+
   /**
-   * Ausgabe des Formulars
-   * @param  string $action Formularwert action
+   * returns a list of input elements that are required but not 
+   * successfully returned
+   *
+   * @return     array  an array list of the empty input elements by their name
+   */
+  public function getEmptyObjects(){
+    if (count($this->emptyObjects)==0)
+      self::check();
+    return $this->emptyObjects;
+  }
+
+
+  /**
+   * Determines if formular was send
+   *
+   * @param      string   $checkVar  The GET/POST variable that must be send
+   * @param      boolean  $get       decision var to decide if $checkVar was send as GET or POST
+   *                                 - true if is GET var - false if is POST var
+   * @param      string   $value     if $checkvar must contain a special string
+   *
+   * @return     boolean  True if available, False otherwise.
+   */
+  public function isAvailable($checkVar="submit",$get=true,$value=""){
+    if     ($get==true  && isset($_GET[$checkVar])  && $_GET[$checkVar]==$value)  return true;
+    else if($get==false && isset($_POST[$checkVar]) && $_POST[$checkVar]==$value) return true;
+    else                                                                          return false;
+  }
+
+
+  /**
+   * outputs the formular
+   *
+   * @param      string  $action  The GET var to prove if formular was send
    */
   public function toHtml($action = "?submit"){
     $inputs=""; $checkObject="";
-    foreach (self::$elements as $name => $value) {
+    foreach ($this->elements as $name => $value) {
       switch ($value['type']) {
         case 'input':    $inputs.=self::input($name);
                          if($value['required']===true)
@@ -139,10 +174,17 @@ class form {
   }
 
 
+  /**
+   * generate the bootstrap toggle by the regex
+   *
+   * @param      name  $name   The name of the form element
+   *
+   * @return     string  the html code for the toggle
+   */
   protected function generateToggle($name){
-    if(self::$elements[$name]['required']==true){
+    if($this->elements[$name]['required']==true){
       $string='data-toggle="popover" title="Information" data-content="';
-      switch (self::$elements[$name]['regex']) {
+      switch ($this->elements[$name]['regex']) {
         case self::FORM_DATE:     return $string.='Bitte geben Sie das Datum im folgenden Format an: dd.mm.yyyy"';
         case self::FORM_PASSWORT: return $string.='Bitte geben Sie ein 8 stelliges Password an welches Buchstaben, Zahlen und Zeichen enthält"';
         case self::FORM_EMAIL:    return $string.='Beispiel: mmustermann@max.de"';
@@ -154,46 +196,17 @@ class form {
       return "";
   }
 
-  /**
-   * prüft, ob alle benötigten Werte übermittelt wurden
-   * @return boolean true, wenn alle Werte richitg übertragen wurden
-   */
-  public function check(){
-    foreach (self::$elements as $name => $value) {
-      if($value['required']===true && (!isset($_POST[$name]) || empty($_POST[$name]) || !preg_match($value['regex'], $_POST[$name]))){
-        self::$emptyObjects[]=$name;
-      }
-    }
-    return (count(self::$emptyObjects)==0);
-  }
 
   /**
-   * gibt die Fehlenden Objecke des Formulars zurück, welche nicht ausgefühlt wurden
-   * @return array 
+   * returns the html code for a file input
+   *
+   * @param      string  $name   The name of the form element
+   *
+   * @return     string  html code
    */
-  public function getEmptyObjects(){
-    return self::$emptyObjects;
-  }
-  /**
-   * prüft, ob ein Formular bereits abgeschickt wurde
-   * @param  string  $checkVar Variablenname der GET oder POST Variable
-   * @param  boolean $get      Übermittlungsmethode: GET oder POST
-   * @param  string  $value    prüf hash, der mit dem Wert der Variable übereinstimmen soll
-   * @return boolean           ist übermittelt: ja oder nein
-   */
-  public function isAvailable($checkVar="submit",$get=true,$value=""){
-    if     ($get==true  && isset($_GET[$checkVar])  && $_GET[$checkVar]==$value)  return true;
-    else if($get==false && isset($_POST[$checkVar]) && $_POST[$checkVar]==$value) return true;
-    else                                                                          return false;
-  }
-
-  /**
-   * Funktion zum Parsen des Datei Eingabefeldes
-   * @param  string $name Index des Formulararrays
-   */
-  public function file($name){
+  protected function file($name){
     $html ='<div class="form-group" id="id-'.$name.'">';
-    $html.='  <label class="col-sm-3 control-label">'.self::$elements[$name]["caption"].'</label>';
+    $html.='  <label class="col-sm-3 control-label">'.$this->elements[$name]["caption"].'</label>';
     $html.='  <div class="col-sm-9">';
     $html.='    <input class="form-control" name="'.$name.'" type="file">';
     $html.='  </div>';
@@ -201,69 +214,89 @@ class form {
     return $html;
   }
 
+  
   /**
-  * Funktion zum Parsen des Input Eingabefeldes
-  * @param  string $name Index des Formulararrays
-  */
-  public function input($name){
+   * returns the html code for a text input
+   *
+   * @param      string  $name   The name of the form element
+   *
+   * @return     string  html code
+   */
+  protected function input($name){
     $html ='<div class="form-group" id="id-'.$name.'">';
-    $html.='  <label class="col-sm-3 control-label">'.self::$elements[$name]["caption"].'</label>';
+    $html.='  <label class="col-sm-3 control-label">'.$this->elements[$name]["caption"].'</label>';
     $html.='  <div class="col-sm-9">';
     $html.='    <input class="form-control" name="'.$name.'" type="text" 
-                       value="'.self::$elements[$name]["value"].'" autocomplete="off" '.self::generateToggle($name).'>';
+                       value="'.$this->elements[$name]["value"].'" autocomplete="off" '.self::generateToggle($name).'>';
     $html.='  </div>';
     $html.='</div>';
     return $html;
   }
 
-  /**
-  * Funktion zum Parsen des Hidden Eingabefeldes
-  * @param  string $name Index des Formulararrays
-  */
-  public function hidden($name){
-    return '<input name="'.$name.'" type="hidden" value="'.self::$elements[$name]["value"].'" autocomplete="off">';
-  }
 
   /**
-  * Funktion zum Parsen des Passwortes Eingabefeldes
-  * @param  string $name Index des Formulararrays
-  */
-  public function password($name){
+   * returns the html code for a hidden input
+   *
+   * @param      string  $name   The name of the form element
+   *
+   * @return     string  html code
+   */
+  protected function hidden($name){
+    return '<input name="'.$name.'" type="hidden" value="'.$this->elements[$name]["value"].'" autocomplete="off">';
+  }
+
+  
+  /**
+   * returns the html code for a password input
+   *
+   * @param      string  $name   The name of the form element
+   *
+   * @return     string  html code
+   */
+  protected function password($name){
     $html ='<div class="form-group" id="id-'.$name.'">';
-    $html.='  <label for="inputPassword" class="col-sm-3 control-label">'.self::$elements[$name]["caption"].'</label>';
+    $html.='  <label for="inputPassword" class="col-sm-3 control-label">'.$this->elements[$name]["caption"].'</label>';
     $html.='  <div class="col-sm-9">';
-    $html.='    <input class="form-control" name="'.$name.'" type="password" value="'.self::$elements[$name]["value"].'" autocomplete="off">';
+    $html.='    <input class="form-control" name="'.$name.'" type="password" value="'.$this->elements[$name]["value"].'" autocomplete="off">';
     $html.='  </div>';
     $html.='</div>';
     return $html;
   }
 
+
   /**
-  * Funktion zum Parsen des Textarea Eingabefeldes
-  * @param  string $name Index des Formulararrays
-  */
-  public function textarea($name){
+   * returns the html code for a textarea input
+   *
+   * @param      string  $name   The name of the form element
+   *
+   * @return     string  html code
+   */
+  protected function textarea($name){
     $html ='<div class="form-group" id="id-'.$name.'">';
-    $html.='  <label class="col-sm-3 control-label">'.self::$elements[$name]["caption"].'</label>';
+    $html.='  <label class="col-sm-3 control-label">'.$this->elements[$name]["caption"].'</label>';
     $html.='  <div class="col-sm-9">';
-    $html.='    <textarea class="form-control" rows="5" name="'.$name.'" '.self::generateToggle($name).'>'.self::$elements[$name]["value"].'</textarea>';
+    $html.='    <textarea class="form-control" rows="5" name="'.$name.'" '.self::generateToggle($name).'>'.$this->elements[$name]["value"].'</textarea>';
     $html.='  </div>';
     $html.='</div>';
     return $html;
   }
 
+
   /**
-  * Funktion zum Parsen einer Liste von Checkboxen Eingabefelder
-  * @param  string $name Index des Formulararrays
-  */
-  public function checkbox($name){
-    if(!is_array(self::$elements[$name]["value"])){
-      self::$elements[$name]["value"]=array(self::$elements[$name]["value"]);
+   * returns the html code for a list of checkboxes
+   *
+   * @param      string  $name   The name of the form element
+   *
+   * @return     string  html code
+   */
+  protected function checkbox($name){
+    if(!is_array($this->elements[$name]["value"])){
+      $this->elements[$name]["value"]=array($this->elements[$name]["value"]);
     }
     $boxes="";
-    foreach (self::$elements[$name]["list"] as $value => $caption) {
+    foreach ($this->elements[$name]["list"] as $value => $caption) {
       $checked="";
-      if(in_array($value, self::$elements[$name]["value"])) $checked="CHECKED";
+      if(in_array($value, $this->elements[$name]["value"])) $checked="CHECKED";
       $boxes.='<div class="checkbox">';
       $boxes.=' <label>';
       $boxes.='   <input type="checkbox" name="'.$name.'[]" value="'.$value.'" '.$checked.'>'.$caption;
@@ -271,7 +304,7 @@ class form {
       $boxes.='</div>';
     }
     $html ='<div class="form-group" id="id-'.$name.'">';
-    $html.='  <label class="col-sm-3 control-label">'.self::$elements[$name]["caption"].'</label>';
+    $html.='  <label class="col-sm-3 control-label">'.$this->elements[$name]["caption"].'</label>';
     $html.='  <div class="col-sm-9">';
     $html.=     $boxes;
     $html.='  </div>';
@@ -279,15 +312,19 @@ class form {
     return $html;
   }
 
+
   /**
-  * Funktion zum Parsen einer Liste von Radio Eingabefelder
-  * @param  string $name Index des Formulararrays
-  */
-  public function radio($name){
+   * returns the html code for a list of radio elements
+   *
+   * @param      string  $name   The name of the form element
+   *
+   * @return     string  html code
+   */
+  protected function radio($name){
     $boxes="";
-    foreach (self::$elements[$name]["list"] as $value => $caption) {
+    foreach ($this->elements[$name]["list"] as $value => $caption) {
       $checked="";
-      if($value==self::$elements[$name]["value"]) $checked="CHECKED";
+      if($value==$this->elements[$name]["value"]) $checked="CHECKED";
       $boxes.='<div class="radio">';
       $boxes.='  <label>';
       $boxes.='    <input type="radio" name="'.$name.'" value="'.$value.'" '.$checked.'>'.$caption;
@@ -295,7 +332,7 @@ class form {
       $boxes.='</div>';
     }
     $html ='<div class="form-group" id="id-'.$name.'">';
-    $html.='  <label class="col-sm-3 control-label">'.self::$elements[$name]["caption"].'</label>';
+    $html.='  <label class="col-sm-3 control-label">'.$this->elements[$name]["caption"].'</label>';
     $html.='  <div class="col-sm-9">';
     $html.=     $boxes;
     $html.='  </div>';
@@ -303,24 +340,28 @@ class form {
     return $html;
   }
 
+
   /**
-  * Funktion zum Parsen des Select Eingabefeldes
-  * @param  string $name Index des Formulararrays
-  */
-  public function select($name){
-    if(isset(self::$elements[$name]["list"][self::$elements[$name]["value"]])){
-      $boxes.='<option value="'.self::$elements[$name]["value"].'">'.
-                self::$elements[$name]["list"][self::$elements[$name]["value"]].'</option>';
-      unset(self::$elements[$name]["list"][self::$elements[$name]["value"]]);
+   * returns the html code for a slect input
+   *
+   * @param      string  $name   The name of the form element
+   *
+   * @return     string  html code
+   */
+  protected function select($name){
+    if(isset($this->elements[$name]["list"][$this->elements[$name]["value"]])){
+      $boxes.='<option value="'.$this->elements[$name]["value"].'">'.
+                $this->elements[$name]["list"][$this->elements[$name]["value"]].'</option>';
+      unset($this->elements[$name]["list"][$this->elements[$name]["value"]]);
     } else {
       $boxes=""; 
     }
-    foreach (self::$elements[$name]["list"] as $value => $caption) {
+    foreach ($this->elements[$name]["list"] as $value => $caption) {
       $boxes.='<option value="'.$value.'">'.$caption.'</option>';
     }
 
     $html ='<div class="form-group" id="id-'.$name.'">';
-    $html.='  <label class="col-sm-3 control-label">'.self::$elements[$name]["caption"].'</label>';
+    $html.='  <label class="col-sm-3 control-label">'.$this->elements[$name]["caption"].'</label>';
     $html.='  <div class="col-sm-9">';
     $html.='    <select class="form-control" name="'.$name.'">';
     $html.=       $boxes;
@@ -331,6 +372,11 @@ class form {
   }
 
 
+  /**
+   * returns the js code that is necessary for the required prove
+   *
+   * @return     string  js code
+   */
   protected function generateJS(){
     $js ='<script type="text/javascript">';
     $js.='  function fieldCheck() {';
